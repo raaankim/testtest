c56137e1ce7c275181ae037922b1d1194e808733
김란 사원 <kran94@seowoninfo.com>
2025-02-24 14:36:45 +0900

    commit

---
+++ src/test/java/org/example/testtest/TesttestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesttestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
